b054d8a99fb3d6a15ca1b9f59deba8588a205feb
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-03-27 22:57:06 +0000

    Spread and Rest Operators refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -59,19 +59,26 @@ const Search = ({ search, onSearch }) => (
 const List = ({list}) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item 
+        key={item.objectID} 
+        title={item.title}
+        url={item.url}
+        author={item.author}
+        num_comments={item.num_comments}
+        points={item.points}
+      />
     ))}
   </ul>
 );
 
-const Item = ({item}) => (
+const Item = ({title, url, author, num_comments, points}) => (
   <li>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span> by {item.author} </span>
-    <span>{item.num_comments} </span>
-    <span>{item.points}</span>
+    <span> by {author} </span>
+    <span>{num_comments} </span>
+    <span>{points}</span>
   </li>
 );
 
