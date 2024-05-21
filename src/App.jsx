4fb704c939bfd8e46d2213309828b0698a5c36c3
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-05-21 20:39:16 +0100

    Styled Components in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import axios from 'axios';
-
-import styles from "./App.module.css"
+import styled from 'styled-compnents'
 
   const storiesReducer = (state, action) => {
     switch (action.type) {
@@ -50,6 +49,85 @@ const useStorageState = (key, initialState) => {
 
 // A - Data Fetching with React
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+const StyledContainer = styled.div` 
+  height: 100vw;
+  padding: 20px;
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
+
+
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState(
@@ -103,8 +181,8 @@ const App = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h1 className= {styles.headlinePrimary}>My Hacker Stories</h1>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 
     <SearchForm
       searchTerm= {searchTerm}
@@ -119,7 +197,7 @@ const App = () => {
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </div>
+    </StyledContainer>
   );
 };
 
@@ -128,7 +206,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
+  <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel
       id= "search"
       value={searchTerm}
@@ -138,14 +216,12 @@ const SearchForm = ({
       <strong> Search: </strong>
     </InputWithLabel>
 
-    <button 
+    <StyledButtonLarge 
     type="submit" 
-    disabled={!searchTerm}
-    className={`${styles.button} ${styles.buttonLarge}` }
-    >
+    disabled={!searchTerm}>
       Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+    </StyledSearchForm>
 );
 
 const InputWithLabel = ({ 
@@ -166,16 +242,14 @@ const InputWithLabel = ({
       
   return (
     <>
-      <label htmlFor={id} className={styles.label}>
-        {children} 
-        </label>  {/* B */}
-      <input 
+      <StyledLabel htmlFor={id}> {children}</StyledLabel>
+         
+      <StyledInput
         ref={inputRef}
         id= {id}
         type={type}
         value={value}
         onChange={onInputChange}
-        className={styles.input}
       />
     </>
   );
@@ -194,21 +268,22 @@ const List = ({ list, onRemoveItem }) => (
 );
 
 const Item = ({ item, onRemoveItem }) => (
-  <li className= {styles.item}>
-    <span style={{ width: '40%' }}>
+  <StyledItem>
+    <StyledColumn width= "40%">
       <a href={item.url}>{item.title}</a>
-    </span>
-    <span style={{ width: '30%' }}>by {item.author} </span>
-    <span style={{ width: '10%' }}>{item.num_comments} </span>
-    <span style={{ width: '10%' }}>{item.points}</span>
-    <span style={{ width: '10%' }}> 
-      <button type="button" onClick= {() => onRemoveItem(item)}
-      className={`${styles.button} ${styles.buttonSmall}`}
+    </StyledColumn>
+    <StyledColumn width= "30%">by {item.author} </StyledColumn>
+    <StyledColumn width= "10%">{item.num_comments} </StyledColumn>
+    <StyledColumn width= "10%">{item.points}</StyledColumn>
+    <StyledColumn width= "10%"> 
+      <StyledButtonSmall 
+      type="button" 
+      onClick= {() => onRemoveItem(item)}
       >
         Dismiss
-      </button>
-    </span>
-  </li>
+      </StyledButtonSmall>
+    </StyledColumn>
+  </StyledItem>
   );
 
 export default App;
